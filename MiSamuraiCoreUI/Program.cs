c31ff5d9f0c30579c1edba2fa5a2821f7ef33157
Adolfo@Entelgy <adolfo.garcia@entelgy.com>
2019-02-22 12:15:49 +0100

    Creacion del Modelo

---
+++ MiSamuraiCoreUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiSamuraiCoreUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MySamurai.Data/SamuraiContext.cs
@@ -0,0 +1,30 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using MySamurai.Domain;
+
+namespace MySamurai.Data
+{
+    public class SamuraiContext: DbContext
+    {
+        public SamuraiContext(DbContextOptions<SamuraiContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+
+        //Contectar con el proveedor
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(
+        //         "Server = (localdb)\\mssqllocaldb; Database = SamuraiAppData; Trusted_Connection = True; ");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });
+
+        }
+
+
+    }
+}
